784a2e763d7cd3f509f0f0c260f6281b5f37f00c
Meghan23 <meghanlendhe@gmail.com>
2019-02-20 23:21:08 +0530

    dish assignment

--- app/src/main/java/com/example/billsplitter/Details.java
+++ app/src/main/java/com/example/billsplitter/Details.java
@@ -15,7 +15,7 @@ import android.widget.TextView;
 
 public class Details extends AppCompatActivity {
 
-    private TextView p1, p2;
+    private TextView item1, item2;
     final Context c = this;
 
     @Override
@@ -23,14 +23,52 @@ public class Details extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         Intent i = getIntent();
-        String pa = i.getStringExtra("ba");
-        String pb = i.getStringExtra("bb");
+        final int[][] det = new int[2][2] ;
+        final String pa = i.getStringExtra("ba");
+        final String pb = i.getStringExtra("bb");
         String ia = i.getStringExtra("i1");
         String ib = i.getStringExtra("i2");
-        p1 = findViewById(R.id.person1);
-        p2 = findViewById(R.id.person2);
+        item1 = findViewById(R.id.i1);
+        item2 = findViewById(R.id.i2);
+        item1.setText(ia);
+        item2.setText(ib);
 
-        p1.setOnClickListener(new View.OnClickListener() {
+        item1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
+                View mView = layoutInflaterAndroid.inflate(R.layout.input_dialogue,null);
+                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
+                alertDialogBuilderUserInput.setView(mView);
+
+                final CheckBox chk1 =  mView.findViewById(R.id.chk1);
+                chk1.setText(pa);
+                final CheckBox chk2 = mView.findViewById(R.id.chk2);
+                chk2.setText(pb);
+                alertDialogBuilderUserInput
+                        .setCancelable(false)
+                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialogBox, int id) {
+                                    if(chk1.isChecked())
+                                        det[0][0] = 1;
+                                    if(chk2.isChecked())
+                                        det[0][1] = 1;
+                            }
+                        })
+
+                        .setNegativeButton("Cancel",
+                                new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialogBox, int id) {
+                                        dialogBox.cancel();
+                                    }
+                                });
+
+                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
+                alertDialogAndroid.show();
+            }
+        });
+
+        item2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
@@ -39,11 +77,17 @@ public class Details extends AppCompatActivity {
                 alertDialogBuilderUserInput.setView(mView);
 
                 final CheckBox chk1 =  mView.findViewById(R.id.chk1);
+                chk1.setText(pa);
+                final CheckBox chk2 = mView.findViewById(R.id.chk2);
+                chk2.setText(pb);
                 alertDialogBuilderUserInput
                         .setCancelable(false)
-                        .setPositiveButton("Send", new DialogInterface.OnClickListener() {
+                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialogBox, int id) {
-                                // ToDo get user input here
+                                if(chk1.isChecked())
+                                    det[1][0] = 1;
+                                if(chk2.isChecked())
+                                    det[1][1] = 1;
                             }
                         })
 
