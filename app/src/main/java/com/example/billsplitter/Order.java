d30ed4addfca907a540d6542cfcf91d55b2be4a9
Meghan23 <meghanlendhe@gmail.com>
2019-02-20 18:22:21 +0530

    people details activity

--- app/src/main/java/com/example/billsplitter/Order.java
+++ app/src/main/java/com/example/billsplitter/Order.java
@@ -1,16 +1,41 @@
 package com.example.billsplitter;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Order extends AppCompatActivity {
 
-    private EditText i1, i2, p1, p2;
+    private EditText ia, ib, pa, pb;
+    private Button next;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_order);
-        i1 = findViewById(R.layout.);
+        ia = findViewById(R.id.itema);
+        pa = findViewById(R.id.price1);
+        ib = findViewById(R.id.item2);
+        pb = findViewById(R.id.price2);
+        next = findViewById(R.id.next);
+        final String i1 = ia.getText().toString();
+        final String i2 = ib.getText().toString();
+        final int p1 = Integer.parseInt(pa.getText().toString());
+        final int p2 = Integer.parseInt(pb.getText().toString());
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(Order.this, People.class);
+                i.putExtra("pa",p1);
+                i.putExtra("pb",p2);
+                i.putExtra("ia",i1);
+                i.putExtra("ib",i2);
+                startActivity(i);
+            }
+        });
+
     }
 }
