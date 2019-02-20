772b61279e19b34b0692c0b61514206767010bd0
Mrunal98 <mrunalkurhade@gmail.com>
2019-02-21 02:38:04 +0530

    XML changes button

--- app/src/main/java/com/example/billsplitter/Order.java
+++ app/src/main/java/com/example/billsplitter/Order.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Order extends AppCompatActivity {
 
@@ -26,20 +27,47 @@ public class Order extends AppCompatActivity {
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String i1 = ia.getText().toString();
-                String i2 = ib.getText().toString();
-                double p1 = Double.parseDouble(pa.getText().toString());
-                double p2 = Double.parseDouble(pb.getText().toString());
-                String b1 = ba.getText().toString();
-                String b2 = bb.getText().toString();
-                Intent i = new Intent(Order.this, Details.class);
-                i.putExtra("pa",p1);
-                i.putExtra("pb",p2);
-                i.putExtra("ia",i1);
-                i.putExtra("ib",i2);
-                i.putExtra("ba",b1);
-                i.putExtra("bb",b2);
-                startActivity(i);
+
+                if(ia.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                if(ib.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                if(pa.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                if(pb.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                if(ba.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                if(bb.getText().toString().equals(""))
+                {
+                    Toast.makeText(Order.this, "Input Text Is Empty.. Please Enter Some Text", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    String i1 = ia.getText().toString();
+                    String i2 = ib.getText().toString();
+                    double p1 = Double.parseDouble(pa.getText().toString());
+                    double p2 = Double.parseDouble(pb.getText().toString());
+                    String b1 = ba.getText().toString();
+                    String b2 = bb.getText().toString();
+                    Intent i = new Intent(Order.this, Details.class);
+                    i.putExtra("pa", p1);
+                    i.putExtra("pb", p2);
+                    i.putExtra("ia", i1);
+                    i.putExtra("ib", i2);
+                    i.putExtra("ba", b1);
+                    i.putExtra("bb", b2);
+                    startActivity(i);
+                }
             }
         });
 
