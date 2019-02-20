6d5b336354ee2cffece38b2d018be41b8294fcc9
Meghan23 <meghanlendhe@gmail.com>
2019-02-21 00:48:12 +0530

    dish assignment(not working)

--- app/src/main/java/com/example/billsplitter/Details.java
+++ app/src/main/java/com/example/billsplitter/Details.java
@@ -1,29 +1,24 @@
 package com.example.billsplitter;
 
-import android.app.AlertDialog;
-import android.app.Dialog;
 import android.content.Context;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.widget.CheckBox;
-import android.widget.EditText;
 import android.widget.TextView;
 
-public class Details extends AppCompatActivity {
+public class Details extends AppCompatActivity implements input_dialog.input_dialogListener {
 
     private TextView item1, item2;
     final Context c = this;
+    final int[][] det = new int[2][2] ;
+    int x;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
         Intent i = getIntent();
-        final int[][] det = new int[2][2] ;
         final String pa = i.getStringExtra("ba");
         final String pb = i.getStringExtra("bb");
         String ia = i.getStringExtra("i1");
@@ -36,72 +31,28 @@ public class Details extends AppCompatActivity {
         item1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
-                View mView = layoutInflaterAndroid.inflate(R.layout.input_dialogue,null);
-                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
-                alertDialogBuilderUserInput.setView(mView);
-
-                final CheckBox chk1 =  mView.findViewById(R.id.chk1);
-                chk1.setText(pa);
-                final CheckBox chk2 = mView.findViewById(R.id.chk2);
-                chk2.setText(pb);
-                alertDialogBuilderUserInput
-                        .setCancelable(false)
-                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogBox, int id) {
-                                    if(chk1.isChecked())
-                                        det[0][0] = 1;
-                                    if(chk2.isChecked())
-                                        det[0][1] = 1;
-                            }
-                        })
-
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogBox, int id) {
-                                        dialogBox.cancel();
-                                    }
-                                });
-
-                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
-                alertDialogAndroid.show();
+                x = 0;
+                openDialog();
             }
         });
 
         item2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                LayoutInflater layoutInflaterAndroid = LayoutInflater.from(c);
-                View mView = layoutInflaterAndroid.inflate(R.layout.input_dialogue, null);
-                AlertDialog.Builder alertDialogBuilderUserInput = new AlertDialog.Builder(c);
-                alertDialogBuilderUserInput.setView(mView);
-
-                final CheckBox chk1 =  mView.findViewById(R.id.chk1);
-                chk1.setText(pa);
-                final CheckBox chk2 = mView.findViewById(R.id.chk2);
-                chk2.setText(pb);
-                alertDialogBuilderUserInput
-                        .setCancelable(false)
-                        .setPositiveButton("Save", new DialogInterface.OnClickListener() {
-                            public void onClick(DialogInterface dialogBox, int id) {
-                                if(chk1.isChecked())
-                                    det[1][0] = 1;
-                                if(chk2.isChecked())
-                                    det[1][1] = 1;
-                            }
-                        })
-
-                        .setNegativeButton("Cancel",
-                                new DialogInterface.OnClickListener() {
-                                    public void onClick(DialogInterface dialogBox, int id) {
-                                        dialogBox.cancel();
-                                    }
-                                });
-
-                AlertDialog alertDialogAndroid = alertDialogBuilderUserInput.create();
-                alertDialogAndroid.show();
+                x = 1;
+                openDialog();
             }
         });
+    }
 
+    public void openDialog(){
+        input_dialog in = new input_dialog();
+        in.show(getSupportFragmentManager(),"checks");
+    }
+
+    @Override
+    public void applyChanges(int a, int b) {
+        det[x][0] = a;
+        det[x][1] = b;
     }
 }
---
+++ app/src/main/java/com/example/billsplitter/input_dialog.java
@@ -0,0 +1,59 @@
+package com.example.billsplitter;
+
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatDialogFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.CheckBox;
+
+public class input_dialog extends AppCompatDialogFragment {
+    int a, b ;
+    private input_dialogListener listener;
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder build = new AlertDialog.Builder(getActivity());
+        LayoutInflater inf = getActivity().getLayoutInflater();
+        final View view = inf.inflate(R.layout.input_dialogue, null);
+        final CheckBox chk1 = view.findViewById(R.id.chk1);
+        final CheckBox chk2 = view.findViewById(R.id.chk2);
+        build.setView(view)
+                .setTitle("Select bois")
+                .setNegativeButton("cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+
+                    }
+                })
+                .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if(chk1.isChecked())
+                            a = 1;
+                        if(chk2.isChecked())
+                            b = 1;
+                        listener.applyChanges(a,b);
+                    }
+                });
+        return build.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try{
+            listener = (input_dialogListener) context;
+        }
+        catch (ClassCastException e){
+            throw new ClassCastException(context.toString() + "must implement inputdialoglistener");
+        }
+
+    }
+
+    public interface input_dialogListener{
+        void applyChanges(int a, int b);
+    }
+}
